fc6abd933c19bc3b4d5eea2159cdeae91ce37110
Vanessa Me Tonini <vanessametonini@gmail.com>
2019-05-28 17:09:42 -0300

    Aula02 metade FormGroup

---
+++ src/app/components/form-group/form-group.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'cmail-form-group',
+  templateUrl: './form-group.component.html',
+  styles: []
+})
+export class FormGroupComponent implements OnInit {
+
+  @Input() label = "";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,5 +6,9 @@ import { Component } from "@angular/core";
   styleUrls: ['./header.component.css', './header-search.css']
 })
 export class HeaderComponent {
+  isMenuOpen = false;
 
+  toggleMenu(){
+    this.isMenuOpen = ! this.isMenuOpen;
+  }
 }
