e84b23aedc5d145e1917cb7db865375cf549f5ca
Vanessa Me Tonini <vanessametonini@gmail.com>
2019-05-28 12:57:37 -0300

    Aula 02 fim manha

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Email } from './models/email';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng8435';
+  
+  private _isEmailOpen = false;
+  email = new Email();
+  listaEmails: Email[] = [];
+
+  get isEmailOpen(){
+    return this._isEmailOpen;
+  }
+
+  toggleEmailForm(){
+    this._isEmailOpen = !this.isEmailOpen;
+  }
+
+  validaCampos(form){
+    for(let nomeControl in form.controls){
+      form.controls[nomeControl].markAsTouched();
+    }
+  }
+
+  enviarEmail(formEmail: NgForm){
+    
+    if(formEmail.invalid){
+      this.validaCampos(formEmail);
+      return
+    }
+
+    this.listaEmails.push(this.email);
+    this.email = new Email();
+    formEmail.reset();
+
+  }
+  
 }
